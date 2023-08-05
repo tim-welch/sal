d46dc9c8e17bf52f1f9932018f48590d33ae7aa9
Tim Welch <tim@welchnet.org>
2023-08-05 10:39:14 -0400

    Evaluate addition

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -1,4 +1,5 @@
 use crate::ast::Expr;
+use crate::scanner::Token;
 use std::error::Error;
 use std::str::FromStr;
 
@@ -7,16 +8,29 @@ use std::str::FromStr;
 // good enough.
 #[derive(Debug, PartialEq)]
 pub enum Value {
-    Number { value: f64 },
+    Number(f64),
 }
 
 pub fn evaluate(expr: &Expr) -> Result<Value, Box<dyn Error>> {
     match expr {
         Expr::NumericLiteral { value } => {
             let value = f64::from_str(value)?;
-            Ok(Value::Number { value })
+            Ok(Value::Number(value))
         }
-        _ => Err("Unsupported expression type".into()),
+        Expr::Binary {
+            left,
+            operator,
+            right,
+        } => {
+            let left = evaluate(left)?;
+            let right = evaluate(right)?;
+            match (operator, left, right) {
+                (Token::Plus, Value::Number(left), Value::Number(right)) => {
+                    Ok(Value::Number(left + right))
+                }
+                _ => Err("Not supported".into()),
+            }
+        }
     }
 }
 
@@ -35,19 +49,57 @@ mod tests {
                 expr: Expr::NumericLiteral {
                     value: String::from("123.345"),
                 },
-                expected: Value::Number { value: 123.345 },
+                expected: Value::Number(123.345),
             },
             Test {
                 expr: Expr::NumericLiteral {
                     value: String::from("0"),
                 },
-                expected: Value::Number { value: 0.0 },
+                expected: Value::Number(0.0),
             },
             Test {
                 expr: Expr::NumericLiteral {
                     value: String::from("0.0"),
                 },
-                expected: Value::Number { value: 0.0 },
+                expected: Value::Number(0.0),
+            },
+        ];
+        for test in tests {
+            let value = evaluate(&test.expr).unwrap();
+            assert_eq!(value, test.expected);
+        }
+    }
+
+    #[test]
+    fn evaluate_addition() {
+        struct Test {
+            expr: Expr,
+            expected: Value,
+        }
+        let tests = vec![
+            Test {
+                expr: Expr::Binary {
+                    left: Box::new(Expr::NumericLiteral {
+                        value: "123.345".into(),
+                    }),
+                    right: Box::new(Expr::NumericLiteral {
+                        value: "1.0".into(),
+                    }),
+                    operator: Token::Plus,
+                },
+                expected: Value::Number(124.345),
+            },
+            Test {
+                expr: Expr::Binary {
+                    left: Box::new(Expr::NumericLiteral {
+                        value: "8753.0".into(),
+                    }),
+                    right: Box::new(Expr::NumericLiteral {
+                        value: "0.0".into(),
+                    }),
+                    operator: Token::Plus,
+                },
+                expected: Value::Number(8753.0),
             },
         ];
         for test in tests {
