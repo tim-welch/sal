a382d238082ab6f900d497aa78860af80d803b2e
Tim Welch <tim@welchnet.org>
2023-08-05 13:49:26 -0400

    Implement repl

--- src/runner.rs
+++ src/runner.rs
@@ -1,11 +1,51 @@
+use crate::ast::parse;
+use crate::interpreter::{evaluate, Value};
+use crate::scanner::tokenize;
+use std::error::Error;
+use std::io;
+use std::io::Write;
+
 pub fn run() {
-    println!("hello, world");
+    loop {
+        print!("> ");
+        io::stdout().flush().unwrap();
+        let mut line = String::default();
+        let res = io::stdin().read_line(&mut line);
+        match res {
+            Ok(_) => match line.as_str().trim() {
+                "quit" => {
+                    break;
+                }
+                _ => match evaluate_line(&line) {
+                    Ok(value) => {
+                        println!("{:?}", value);
+                    }
+                    Err(err) => {
+                        println!("{}", err);
+                    }
+                },
+            },
+            Err(err) => {
+                println!("{}", err);
+            }
+        }
+    }
+}
+
+pub fn evaluate_line(line: &str) -> Result<Value, Box<dyn Error>> {
+    let tokens = tokenize(line)?;
+    let ast = parse(&tokens)?;
+    let value = evaluate(&ast)?;
+    Ok(value)
 }
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
     fn sanity() {
-        assert_eq!(1 + 1, 2);
+        let value = evaluate_line("10 + 2 + 3 * 9 - 4").unwrap();
+        assert_eq!(value, Value::Number(35.0));
     }
 }
